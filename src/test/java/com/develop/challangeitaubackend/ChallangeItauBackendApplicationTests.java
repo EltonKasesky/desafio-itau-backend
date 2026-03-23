2878e256e3fa5d602a56b43377744d254a4b862a
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2026-03-23 16:26:26 -0300

    Start spring boot java

---
+++ src/test/java/com/develop/challangeitaubackend/ChallangeItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.challangeitaubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeItauBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
