7fc52d257e52d734f351370f0008065b27102326
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2026-03-25 18:26:55 -0300

    apply lombok at transaction entity

--- src/main/java/com/develop/challangeitaubackend/modules/transactions/entity/TransactionEntity.java
+++ src/main/java/com/develop/challangeitaubackend/modules/transactions/entity/TransactionEntity.java
@@ -1,7 +1,14 @@
 package com.develop.challangeitaubackend.modules.transactions.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.OffsetDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TransactionEntity {
     private Double valor;
     private OffsetDateTime dataHora;
