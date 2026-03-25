ec1853b9df8e1299bb6097f98804d662fa483d26
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2026-03-25 18:42:46 -0300

    create transaction controller with add transaction method

---
+++ src/main/java/com/develop/challangeitaubackend/modules/transactions/controller/TransactionController.java
@@ -0,0 +1,34 @@
+package com.develop.challangeitaubackend.modules.transactions.controller;
+
+import com.develop.challangeitaubackend.modules.transactions.dto.TransactionRequestDTO;
+import com.develop.challangeitaubackend.modules.transactions.exception.TransactionInFutureException;
+import com.develop.challangeitaubackend.modules.transactions.exception.TransactionNegativeException;
+import com.develop.challangeitaubackend.modules.transactions.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransactionController {
+    private final TransactionService service;
+
+    public TransactionController(TransactionService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addTransaction(@Valid @RequestBody TransactionRequestDTO dto){
+        try {
+            service.addTransaction(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (TransactionNegativeException e){
+            return ResponseEntity.unprocessableEntity().build();
+        } catch (TransactionInFutureException e){
+            return ResponseEntity.unprocessableEntity().build();
+        } catch (RuntimeException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
