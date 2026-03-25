7b6597abc16b8c318cfd80e7652307282f4e97a0
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2026-03-25 18:25:53 -0300

    create transaction entity

---
+++ src/main/java/com/develop/challangeitaubackend/modules/transactions/entity/TransactionEntity.java
@@ -0,0 +1,8 @@
+package com.develop.challangeitaubackend.modules.transactions.entity;
+
+import java.time.OffsetDateTime;
+
+public class TransactionEntity {
+    private Double valor;
+    private OffsetDateTime dataHora;
+}
