c4808d7466c45d33850acea007ca2733fcf16e23
Elton Giglio Kasesky <eltonkasesky9@gmail.com>
2026-03-25 18:46:36 -0300

    remove lombok from dto

--- src/main/java/com/develop/challangeitaubackend/modules/transactions/dto/TransactionRequestDTO.java
+++ src/main/java/com/develop/challangeitaubackend/modules/transactions/dto/TransactionRequestDTO.java
@@ -1,7 +1,5 @@
 package com.develop.challangeitaubackend.modules.transactions.dto;
 
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,9 +8,6 @@ import java.time.OffsetDateTime;
 @AllArgsConstructor
 @Data
 public class TransactionRequestDTO {
-    @Positive(message = "O valor da transação deve ser maior que zero.")
     private Double valor;
-
-    @PastOrPresent(message = "A transação não pode ser feita no futuro.")
     private OffsetDateTime dataHora;
 }
